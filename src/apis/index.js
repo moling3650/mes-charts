b2426ce162f3b5773aa74d23433c0cb411babba7
moling <365024424@qq.com>
2018-02-26 10:24:11 +0800

    更新模拟api的数据

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,3 +1,24 @@
 export function fetchData () {
-  return Promise.resolve([1, 2, 3, 4, 5])
+  return Promise.resolve([
+    {
+      label: 'A',
+      value: 1
+    },
+    {
+      label: 'B',
+      value: 2
+    },
+    {
+      label: 'C',
+      value: 3
+    },
+    {
+      label: 'D',
+      value: 4
+    },
+    {
+      label: 'E',
+      value: 5
+    }
+  ])
 }

b56b19a89e59e8ac5f303e56e37953dff42ccc56
moling <365024424@qq.com>
2018-03-05 14:47:47 +0800

    重构柱状图组件

--- src/components/Bar.vue
+++ src/components/Bar.vue
@@ -7,34 +7,54 @@ const echarts = require('echarts/lib/echarts')
 // 引入柱状图
 require('echarts/lib/chart/bar')
 // 引入提示框和标题组件
-require('echarts/lib/component/tooltip')
 require('echarts/lib/component/title')
+require('echarts/lib/component/tooltip')
+require('echarts/lib/component/legend')
 export default {
   name: 'Bar',
   props: {
     data: {
       type: Array,
-      required: true
+      default: () => []
     }
   },
-  mounted () {
-    const bar = echarts.init(this.$refs.bar)
-    setTimeout(() => {
-      bar.setOption({
+  data () {
+    return {
+      bar: null
+    }
+  },
+  watch: {
+    'data': 'refresh'
+  },
+  methods: {
+    refresh () {
+      if (!this.data.length) {
+        return
+      }
+      const series = Object.keys(this.data[0]).slice(1).map(key => {
+        return {
+          name: key,
+          type: 'bar',
+          data: this.data.map(item => item[key])
+        }
+      })
+
+      this.bar.setOption({
         title: {
-          text: 'ECharts 示例'
+          text: 'ECharts 柱状图'
         },
         tooltip: {},
+        legend: {},
         xAxis: {
           data: this.data.map(item => Object.values(item)[0])
         },
         yAxis: {},
-        series: [{
-          type: 'bar',
-          data: this.data
-        }]
+        series
       })
-    }, 20)
+    }
+  },
+  mounted () {
+    this.bar = echarts.init(this.$refs.bar)
   }
 }
 </script>
